e2636cba0871054dedcadcb1d883b3237f6eb8ee
osija <kaydarovs@mail.ru>
2021-05-06 15:52:44 +0300

    trim quotes

--- Program.cs
+++ Program.cs
@@ -10,31 +10,65 @@ namespace ImgExtractor
     {
         static async Task Main(string[] args)
         {
-
             string sourcePath = "";
-            string targetPath = "files";
+            string targetFolder = "files";
+            string targetPath = targetFolder;
+            char[] charsToTrimForPath = new char[]{'\'', '\"'};
             
             if(args == null || args.Length < 1)
             {
-                Console.WriteLine("Insert a folder with files : ");
-                sourcePath = Console.ReadLine().Replace(" ", "").Replace("\"","").Replace("\'", "");
+                Console.WriteLine("Path to files folder: ");
+                try
+                {
+                    sourcePath = Console.ReadLine().Trim(charsToTrimForPath);
+                    if(!Directory.Exists(sourcePath))
+                    {
+                        throw new DirectoryNotFoundException();
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+                
             }
             else
             {
                 try
                 {
                     if(args[0] != null)
-                    sourcePath = args[0];
+                    {
+                        sourcePath = args[0].Trim(charsToTrimForPath);
+                        if(!Directory.Exists(sourcePath))
+                        {
+                            throw new DirectoryNotFoundException();
+                        }
+                    }
 
                     if(args.Length > 1)
+                    {
                         if(args[1] != null)
-                            targetPath = args[1].Replace(" ", "").Replace("\"","").Replace("\'", "") + "/" + targetPath;
+                        {
+                             targetPath = args[1].Trim(charsToTrimForPath) + "/" + targetFolder;
+                             try
+                             {
+                                 Directory.CreateDirectory(targetPath);
+                             }
+                             catch(Exception e)
+                             {
+                                 Console.WriteLine(e.Message);
+                                 return;
+                             }
+                        }
+                           
+                    }
+                        
                 }
                 catch(Exception e)
                 {
                     Console.WriteLine(e.Message);
                 }
-                
                 
             }
 
@@ -64,7 +98,7 @@ namespace ImgExtractor
                 await map.searchFilesAsync();
                 printMap(map.getAmountMap());
                 await map.copyAsync();
-                //map.saveLog();
+                map.saveLog();
             }   
             catch(Exception E)
             {
